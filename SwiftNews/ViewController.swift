34456c74f901b9e45c0c074ec27751d66bd155e8
verma-aakash <57582868+verma-aakash@users.noreply.github.com>
2020-11-19 09:25:02 -0500

    Initial Commit

---
+++ SwiftNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNews
+//
+//  Created by Aakash Verma on 2020-11-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
